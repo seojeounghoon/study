ff6465b514b564a3b217816d2e893d4d3367cc78
seo7777 <tjwjdvkf7777@gmail.com>
2020-04-08 21:17:24 +0900

    Method

---
+++ csharp_study/03_Function/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Function
+{
+
+    class Calculator
+    {
+        public static int Plus(int a , int b)
+        {
+            return a + b;
+        }
+
+        public static int Minus(int a , int b)
+        {
+            return a - b;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Calculator.Plus(10, 4);
+            Console.WriteLine(result);
+
+
+            result = Calculator.Minus(5, 2);
+            Console.WriteLine(result);
+
+
+
+        }
+    }
+}
