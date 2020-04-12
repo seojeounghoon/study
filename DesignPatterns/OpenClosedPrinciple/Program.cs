2889e0a9b4ee6ab291178b02a800b7d784884c1d
seo7777 <tjwjdvkf7777@gmail.com>
2020-04-12 21:33:35 +0900

    OpenClosedPrinciple

---
+++ DesignPatterns/OpenClosedPrinciple/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace OpenClosedPrinciple
+{
+    public enum Color
+    {
+        Red, Green, Blue
+    }
+
+    public enum Size
+    {
+        Small, Medium, Large
+    }
+
+    public class Product
+    {
+        public string Name;
+        public Color Color;
+        public Size Size;
+
+        public Product(string Name, Color Color, Size Size)
+        {
+            this.Name = Name;
+            this.Color = Color;
+            this.Size = Size;
+        }
+
+    }
+
+    public class ProductFilter
+    {
+        public  IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+        {
+            foreach (var item in products)
+                if (item.Size == size)
+                    yield return item;
+        }
+
+        public  IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+        {
+            foreach (var item in products)
+                if (item.Color == color)
+                    yield return item;
+        }
+    }
+
+
+    
+    
+    
+    
+    
+    // new
+    public interface ISpecification<T>
+    {
+        bool IsSatisfied(Product p);
+    }
+    public interface IFilter<T>
+    {
+        IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> spec);
+    }
+
+    public class ColorSpecification : ISpecification<Product>
+    {
+        private Color color;
+
+        public ColorSpecification(Color color)
+        {
+            this.color = color;
+        }
+
+        public bool IsSatisfied(Product p)
+        {
+            return p.Color == color;
+        }
+    }
+
+    public class SizeSpecification : ISpecification<Product>
+    {
+        private Size size;
+
+        public SizeSpecification(Size size)
+        {
+            this.size = size;
+        }
+
+        public bool IsSatisfied(Product p)
+        {
+            return p.Size == size;
+        }
+    }
+
+    // combinator
+    public class AndSpecification<T> : ISpecification<T>
+    {
+        private ISpecification<T> first, second;
+
+        public AndSpecification(ISpecification<T> first, ISpecification<T> second)
+        {
+            this.first = first; 
+            this.second = second; 
+        }
+
+        public bool IsSatisfied(Product p)
+        {
+            return first.IsSatisfied(p) && second.IsSatisfied(p);
+        }
+    }
+
+    public class BetterFilter : IFilter<Product>
+    {
+        public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> spec)
+        {
+            foreach (var i in items)
+                if (spec.IsSatisfied(i))
+                    yield return i;
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var apple = new Product("apple", Color.Green, Size.Small);
+            var tree = new Product("tree", Color.Green, Size.Large);
+            var house = new Product("house", Color.Blue, Size.Large);
+
+            Product[] products = { apple, tree, house };
+
+            var pf = new ProductFilter();
+            Console.WriteLine("Green Product (Old)");
+            foreach (var p in pf.FilterByColor(products, Color.Green))
+            {
+                WriteLine($" - { p.Name} is Green");
+            }
+
+            //
+            //  AFTER
+
+
+
+
+            var bf = new BetterFilter();
+            WriteLine("Green products (new):");
+            foreach (var p in bf.Filter(products, new ColorSpecification(Color.Green)))
+                WriteLine($" - {p.Name} is green");
+
+            WriteLine("Large products");
+            foreach (var p in bf.Filter(products, new SizeSpecification(Size.Large)))
+                WriteLine($" - {p.Name} is large");
+
+            WriteLine("Large blue items");
+            foreach (var p in bf.Filter(products,
+              new AndSpecification<Product>(new ColorSpecification(Color.Blue), new SizeSpecification(Size.Large)))
+            )
+            {
+                WriteLine($" - {p.Name} is big and blue");
+            }
+
+
+        }
+    }
+}
